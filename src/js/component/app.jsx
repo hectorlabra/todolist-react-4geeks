36338f64df00810c96e0727f7ebab3859639b5e7
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-28 18:06:20 -0400

    first version of counter items

---
+++ src/js/component/app.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import TaskList from "./TaskList.jsx";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      newTaskText: "",
+      tasksLeft: 0, // Nuevo estado para el contador
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ newTaskText: event.target.value });
+  };
+
+  handleTaskAdd = (event) => {
+    event.preventDefault();
+    const { tasks, newTaskText } = this.state;
+    if (newTaskText.trim() !== "") {
+      const newTask = {
+        id: Date.now(),
+        text: newTaskText,
+        active: true,
+      };
+      this.setState((prevState) => ({
+        tasks: [...prevState.tasks, newTask],
+        newTaskText: "",
+        tasksLeft: prevState.tasksLeft + 1, // Incrementar el contador
+      }));
+    }
+  };
+
+  handleTaskToggle = (taskId) => {
+    const updatedTasks = this.state.tasks.map((task) => {
+      if (task.id === taskId) {
+        task.active = !task.active;
+      }
+      return task;
+    });
+
+    // Verifica si la tarea pasó de activa a completada
+    const completedTask = this.state.tasks.find((task) => task.id === taskId);
+    if (completedTask && !completedTask.active) {
+      this.setState((prevState) => ({
+        tasks: updatedTasks,
+        tasksLeft: prevState.tasksLeft - 1, // Decrementa el contador
+      }));
+    } else {
+      this.setState({ tasks: updatedTasks });
+    }
+  };
+
+  handleTaskDelete = (taskId) => {
+    const { tasks } = this.state;
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    this.setState((prevState) => ({
+      tasks: updatedTasks,
+      tasksLeft: prevState.tasksLeft - 1, // Decrementa el contador
+    }));
+  };
+
+  render() {
+    const { tasks, newTaskText, tasksLeft } = this.state;
+    return (
+      <div className="container mt-5">
+        <h1>TodoList</h1>
+        <form onSubmit={this.handleTaskAdd}>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Nueva tarea"
+              value={newTaskText}
+              onChange={this.handleInputChange}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" type="submit">
+                Agregar
+              </button>
+            </div>
+          </div>
+        </form>
+        <TaskList
+          tasks={tasks}
+          onTaskToggle={this.handleTaskToggle}
+          onTaskDelete={this.handleTaskDelete}
+        />
+        {tasksLeft === 1 ? <p>1 Item left</p> : <p>{tasksLeft} Items left</p>}
+      </div>
+    );
+  }
+}
+
+export default App;

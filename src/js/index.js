45bfbcd796c81a2fcc38331717ea57454c176e37
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-28 17:58:19 -0400

    import component on index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
-//import react into the bundle
+// Importa React y ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./component/app";
 
-// include your styles into the webpack bundle
+// Incluye tus estilos en el bundle de Webpack
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// Renderiza tu aplicación React
+ReactDOM.render(<App />, document.querySelector("#app"));

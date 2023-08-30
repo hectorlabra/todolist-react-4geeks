f4e4bdc5d98f756f10c24be36037bcf5f7f79c23
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-30 15:50:38 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,7 @@
-// Importa React y ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/app";
 
-// Incluye tus estilos en el bundle de Webpack
 import "../styles/index.css";
 
-// Renderiza tu aplicación React
 ReactDOM.render(<App />, document.querySelector("#app"));
